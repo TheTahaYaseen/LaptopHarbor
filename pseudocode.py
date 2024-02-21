63e42376ab3e311ab492a8a990c0b8e2c4ef126d
The Taha Yaseen <thetahayaseen@gmail.com>
2024-02-21 22:42:49 +0500

    Plan For Auth

--- pseudocode.py
+++ pseudocode.py
@@ -1,7 +1,23 @@
+"""
+
 # My Draft Of What The App Will Be (My Own Renowned Version)
+- Setup Of API Constants And All That At First
 
 """
 
+# Splash Screen 
 
+"""
+
+Just Display The Name With Some Sort Of Slogan And Description
+
+"""
+
+# User Registration, Login And Logout
+
+"""
+
+Frontend UI: Following The UI But With Proper Convention And Validation Upon Submission
+Django APIs: Standard Auth For Register And Login But No Action Actually Happening Except Validation
 
 """
